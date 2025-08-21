be73e2ffd41c6df0a6eff1f1d4dca947fd5fb1b3
Andy Clemenko <clemenko@gmail.com>
2025-08-21 09:13:34 -0700

    playing with colors

--- src/lib/components/Google.svelte
+++ src/lib/components/Google.svelte
@@ -4,13 +4,13 @@
 </script>
 
 <label class="font-['Open_Sans'] text-8xl font-normal" for={f}>
-	<span class="text-blue-500 dark:text-blue-400">L</span><span
-		class="text-red-500 dark:text-red-400">m</span
-	><span class="text-amber-500 dark:text-amber-400">g</span><span
-		class="text-blue-500 dark:text-blue-400">t</span
-	><span class="text-green-500 dark:text-green-400">f</span><span
-		class="text-red-500 dark:text-red-400">y</span
-	>
+		<span class="text-blue-500 dark:text-blue-400">L</span><span
+			class="text-blue-500 dark:text-blue-400">m</span
+		><span class="text-blue-500 dark:text-blue-400">g</span><span
+			class="text-blue-500 dark:text-blue-400">t</span
+		><span class="text-blue-500 dark:text-blue-400">f</span><span
+			class="text-blue-500 dark:text-blue-400">y</span
+		>
 </label>
 
 <label for={f}><small>Let me Glean that for you</small></label>
